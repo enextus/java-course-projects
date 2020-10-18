eafc84e2e7dc13a7f85edb30c20633b1ad0b5a9e
enextus <enextus@gmail.com>
2020-10-18 23:52:31 +0200

    hw028 10

---
+++ src/day028/hw028/CheckHowTimesAppearChars.java
@@ -0,0 +1,28 @@
+package day028.hw028;
+
+public class CheckHowTimesAppearChars {
+
+    /**
+     * Return true if the string “cat” and “dog” appear the same number of times in the given string.
+     *
+     * Expectation:
+     * catDog("catdog") → true
+     * catDog("catcat") → false
+     * catDog("1cat1cadodog") → true
+     *
+     * Test:
+     *
+     */
+
+    public static void main(String[] args) {
+
+        catDog("catdog");
+        catDog("catcat");
+        catDog("1cat1cadodog");
+    }
+
+    private static boolean catDog(String str) {
+
+        return false;
+    }
+}
