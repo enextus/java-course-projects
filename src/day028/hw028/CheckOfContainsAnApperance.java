27eaaf8105f7e1c392e5144a5324792d701c0c34
enextus <enextus@gmail.com>
2020-10-19 00:49:47 +0200

    hw028 32

---
+++ src/day028/hw028/CheckOfContainsAnApperance.java
@@ -0,0 +1,29 @@
+package day028.hw028;
+
+public class CheckOfContainsAnApperance {
+
+    /**
+     * Return true if the given string contains an appearance of “xyz”
+     * where the xyz is not directly preceeded by a period (.).
+     * So “xxyz” counts but “x.xyz” does not.
+     * <p>
+     * Expectation:
+     * xyzThere("abcxyz") → true
+     * xyzThere("abc.xyz") → false
+     * xyzThere("xyz.abc") → true
+     * <p>
+     * Test:
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println(xyzThere("abcxyz"));
+        System.out.println(xyzThere("abc.xyz"));
+        System.out.println(xyzThere("xyz.abc"));
+    }
+
+    private static boolean xyzThere(String abcxyz) {
+
+        return false;
+    }
+}
