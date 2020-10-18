87ddf04fca101d678e05b6bbdf195db8c49bb28c
enextus <enextus@gmail.com>
2020-10-19 00:01:17 +0200

    hw028 14

--- src/day028/hw028/CheckHowTimesAppearChars.java
+++ src/day028/hw028/CheckHowTimesAppearChars.java
@@ -4,21 +4,20 @@ public class CheckHowTimesAppearChars {
 
     /**
      * Return true if the string “cat” and “dog” appear the same number of times in the given string.
-     *
+     * <p>
      * Expectation:
      * catDog("catdog") → true
      * catDog("catcat") → false
      * catDog("1cat1cadodog") → true
-     *
+     * <p>
      * Test:
-     *
      */
 
     public static void main(String[] args) {
 
         catDog("catdog");
-        catDog("catcat");
-        catDog("1cat1cadodog");
+        //catDog("catcat");
+        //catDog("1cat1cadodog");
     }
 
     private static boolean catDog(String str) {
@@ -26,7 +25,18 @@ public class CheckHowTimesAppearChars {
         String catPattern = "cat";
         String dogPattern = "dog";
 
+        int count = 0;
+
         // str.contains(str11)
+
+        do {
+
+            System.out.println(str);
+
+            count ++;
+
+        } while ( count < str.length());
+
 
         return false;
     }
