1ab0691a43eb4a3a1c6fb475ca966ec132ba00ba
enextus <enextus@gmail.com>
2020-10-19 07:36:34 +0200

    hw028 36

--- src/day028/hw028/CheckOfContainsAnApperance.java
+++ src/day028/hw028/CheckOfContainsAnApperance.java
@@ -32,9 +32,8 @@ public class CheckOfContainsAnApperance {
 
         int index = str.indexOf(".xyz");
 
-        System.out.println("index: " + index);
-
         if (index >= 0) return xyzThere(str.substring(0, index)) || xyzThere(str.substring(index + 4));
+
         else return (str.contains("xyz"));
     }
 }
