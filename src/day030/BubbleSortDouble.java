52d8532f9e8da068fb30cea0cfb543e777175351
enextus <enextus@gmail.com>
2020-10-19 13:12:31 +0200

    Bubble sorting example 3

--- src/day030/BubbleSortDouble.java
+++ src/day030/BubbleSortDouble.java
@@ -28,9 +28,7 @@ public class BubbleSortDouble {
 
         for (int i = 0; i < arrUnsorted.length - 1; i++) {
 
-            if (arrUnsorted[i] <= arrUnsorted[i + 1]) {
-                continue;
-            }
+            if (arrUnsorted[i] <= arrUnsorted[i + 1]) continue;
 
             double temp = arrUnsorted[i];
             arrUnsorted[i] = arrUnsorted[i + 1];
@@ -39,9 +37,7 @@ public class BubbleSortDouble {
             count++;
         }
 
-        if (count > 0) {
-            bubbleSortDouble(arrUnsorted);
-        }
+        if (count > 0) bubbleSortDouble(arrUnsorted);
     }
 
     private static double[] copyArray(double[] input) {
