87e89451806ec67c3c054fe8b82af810ef2a0932
enextus <enextus@gmail.com>
2020-10-21 12:44:08 +0200

    cw032 rev

--- src/day032/Classwork01.java
+++ src/day032/Classwork01.java
@@ -10,6 +10,28 @@ public class Classwork01 {
 
         //
 
+        int smallBars = 0;
+        int bigBars = 0;
+        int goal = 15;
 
+        System.out.println(makeChocolate(smallBars, bigBars, goal));
+    }
+
+    public static int makeChocolate(int small, int big, int goal) {
+
+        int rem = goal % 5;
+
+        if (small + (big * 5) < goal)
+            return -1;
+
+        else if (rem <= small && goal - big * 5 > 4)
+            return rem + 5;
+
+        else if (rem <= small)
+            return rem;
+        else
+            return -1;
     }
 }
+
+
