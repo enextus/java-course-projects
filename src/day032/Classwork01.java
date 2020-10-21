57cab59debccd39232ae0a56acf25af27dbd2021
enextus <enextus@gmail.com>
2020-10-21 13:03:14 +0200

    cw032 rev

--- src/day032/Classwork01.java
+++ src/day032/Classwork01.java
@@ -3,7 +3,13 @@ package day032;
 public class Classwork01 {
 
     /**
-     * classwork
+     * classwork makeChocolate
+     *
+     * We want make a package of goal kilos of chocolate.
+     * We have small bars (1 kilo each) and big bars (5 kilos each).
+     * Return the number of small bars to use, assuming we always use big bars before small bars.
+     * Return -1 if it can't be done.
+     *
      */
 
     public static void main(String[] args) {
