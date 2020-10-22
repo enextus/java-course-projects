4a4f8f5515e2f2465c09bcebcec2b95f3ef6f4d2
enextus <enextus@gmail.com>
2020-10-22 20:37:53 +0200

    hanoi towers 01

---
+++ src/day033/hw033/TheTowersOfHanoi.java
@@ -0,0 +1,12 @@
+package day033.hw033;
+
+public class TheTowersOfHanoi {
+
+    /**
+     * The Towers of Hanoi - one of possible  solution
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
