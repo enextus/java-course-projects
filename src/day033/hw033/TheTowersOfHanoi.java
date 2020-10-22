42d40c14c6093cf4b920442e4b92c1a373433c03
enextus <enextus@gmail.com>
2020-10-22 20:57:32 +0200

    hanoi towers 06

--- src/day033/hw033/TheTowersOfHanoi.java
+++ src/day033/hw033/TheTowersOfHanoi.java
@@ -18,6 +18,7 @@ public class TheTowersOfHanoi {
      * *
      * With 3 disks, the puzzle can be solved in 7 moves. The minimal number of moves required to solve
      * a Tower of Hanoi puzzle is 2^n – 1, where n is the number of disks.
+     * For example, a stack of 20 disks will require 2^20 – 1 moves;
      */
 
     public static void main(String[] args) {
