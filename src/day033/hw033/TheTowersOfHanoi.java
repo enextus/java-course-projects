50d0a9bee70ef5b182faae1785480c341e4bf710
enextus <enextus@gmail.com>
2020-10-22 20:55:48 +0200

    hanoi towers 03

--- src/day033/hw033/TheTowersOfHanoi.java
+++ src/day033/hw033/TheTowersOfHanoi.java
@@ -8,17 +8,16 @@ public class TheTowersOfHanoi {
      * is a mathematical game or puzzle. It consists of three rods and a number of disks of different sizes,
      * which can slide onto any rod. The puzzle starts with the disks in a neat stack in ascending order of size
      * on one rod, the smallest at the top, thus making a conical shape.
-     *
+     * *
      * The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
-     *
-     *     - Only one disk can be moved at a time.
-     *     - Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack
-     *       or on an empty rod.
-     *     - No larger disk may be placed on top of a smaller disk.
-     *
+     * *
+     * - Only one disk can be moved at a time.
+     * - No larger disk may be placed on top of a smaller disk.
+     * - Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack
+     * or on an empty rod.
+     * *
      * With 3 disks, the puzzle can be solved in 7 moves. The minimal number of moves required to solve
-     * a Tower of Hanoi puzzle is 2n − 1, where n is the number of disks.
-     *
+     * a Tower of Hanoi puzzle is 2^n – 1, where n is the number of disks.
      */
 
     public static void main(String[] args) {
