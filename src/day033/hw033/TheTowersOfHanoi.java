487cedca35c5b20048f81eb1eeb93e3caf853038
enextus <enextus@gmail.com>
2020-10-22 20:41:00 +0200

    hanoi towers 02

--- src/day033/hw033/TheTowersOfHanoi.java
+++ src/day033/hw033/TheTowersOfHanoi.java
@@ -4,6 +4,21 @@ public class TheTowersOfHanoi {
 
     /**
      * The Towers of Hanoi - one of possible  solution
+     * The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower[1] and sometimes pluralized as Towers)
+     * is a mathematical game or puzzle. It consists of three rods and a number of disks of different sizes,
+     * which can slide onto any rod. The puzzle starts with the disks in a neat stack in ascending order of size
+     * on one rod, the smallest at the top, thus making a conical shape.
+     *
+     * The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+     *
+     *     - Only one disk can be moved at a time.
+     *     - Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack
+     *       or on an empty rod.
+     *     - No larger disk may be placed on top of a smaller disk.
+     *
+     * With 3 disks, the puzzle can be solved in 7 moves. The minimal number of moves required to solve
+     * a Tower of Hanoi puzzle is 2n − 1, where n is the number of disks.
+     *
      */
 
     public static void main(String[] args) {
