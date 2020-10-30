3437389dbdc50c1d3765c5f190726ad7743cc041
enextus <enextus@gmail.com>
2020-10-30 21:15:54 +0100

    bashscript

---
+++ src/linux/homework/3/myfirstbashscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#var USER2 will be created
+USER2=Eduard
+# print date
+echo -n Today is:; date;
+# print hello
+echo Hello $USER2!;
+# print working directory
+echo -n Working directory:; pwd;
