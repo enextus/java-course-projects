e794e5280caae26792c0370ff23389d5923e1b0a
enextus <enextus@gmail.com>
2020-10-30 21:47:00 +0100

    bashscript

--- src/linux/homework/3/myfirstbashscript.sh
+++ src/linux/homework/3/myfirstbashscript.sh
@@ -8,3 +8,11 @@ echo -n Today is:; date;
 echo Hello $USER2!;
 # print working directory
 echo -n Working directory:; pwd;
+# print number of processes
+ps -ef | tail +2 | wc -l
+# print number of bioset process
+ps -ef | grep bioset | tail +2 | wc -l
+# print chmod of bioset process /etc/passwd
+ls -la /etc/passwd | awk {'print $1'}
+
+
