22430f7aa2c8a9a2b2d8245771432dedf34cf0c4
enextus <enextus@gmail.com>
2020-11-02 07:54:25 +0100

    checkRightUpperLeftDownCorner upd

--- src/day034/MinesweeperInt.java
+++ src/day034/MinesweeperInt.java
@@ -84,10 +84,10 @@ public class MinesweeperInt {
 
     private static void checkRightUpperLeftDownCorner(int[][] arr, int y, int x) {
 
-        if (y >= 1 && x >= 1)
+        if (y >= 1 && (x >= 1 && x < arr.length - 1))
             if (arr[y - 1][x + 1] != -1) arr[y - 1][x + 1] = arr[y - 1][x + 1] + 1;
 
-        if (y < arr.length - 1 && x < arr.length - 1)
+        if (y < arr.length - 1 && (x >= 1 && x < arr.length - 1))
             if (arr[y + 1][x - 1] != -1) arr[y + 1][x - 1] = arr[y + 1][x - 1] + 1;
     }
 
