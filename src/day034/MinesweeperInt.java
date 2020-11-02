73d5438ae1bd8da242a21ce5a638461ba7b246af
enextus <enextus@gmail.com>
2020-11-02 07:15:32 +0100

    checkLeftUpperRightDownCorner

--- src/day034/MinesweeperInt.java
+++ src/day034/MinesweeperInt.java
@@ -71,6 +71,25 @@ public class MinesweeperInt {
             if (arr[y + 1][x] != -1) arr[y + 1][x] = arr[y + 1][x] + 1;
     }
 
+    private static void checkLeftUpperRightDownCorner(int[][] arr, int y, int x) {
+
+        if (y >= 1)
+            if (arr[y - 1][x - 1] != -1) arr[y - 1][x - 1] = arr[y - 1][x - 1] + 1;
+
+        if (y < arr.length - 1)
+            if (arr[y + 1][x + 1] != -1) arr[y + 1][x + 1] = arr[y + 1][x + 1] + 1;
+    }
+
+    private static void checkRightUpperLeftDownCorner(int[][] arr, int y, int x) {
+
+        if (y >= 1)
+            if (arr[y - 1][x] != -1) arr[y - 1][x] = arr[y - 1][x] + 1;
+
+        if (y < arr.length - 1)
+            if (arr[y + 1][x] != -1) arr[y + 1][x] = arr[y + 1][x] + 1;
+    }
+
+
     private static int[][] createMinefield(int size) {
 
         int[][] output = new int[size][size];
