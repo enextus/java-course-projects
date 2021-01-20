bcab6591b1cda9c84bae770f2a6cf872dacfea68
enextus <enextus@gmail.com>
2021-01-20 19:50:01 +0100

    some upd

--- src/day021/hw021/MakeArrayWithPrimeNumbersFromRange.java
+++ src/day021/hw021/MakeArrayWithPrimeNumbersFromRange.java
@@ -7,20 +7,19 @@ public class MakeArrayWithPrimeNumbersFromRange {
      * create an array with prime numbers in this range.
      * *
      * Test:
-     *         int a = 30;
-     *         int b = 50;
-     *         Array int[] {31, 37, 41, 43, 47}.
-     *
-     *         int a = 20;
-     *         int b = 93;
-     *         Array int[] {23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89}.
-     *
+     * int a = 30;
+     * int b = 50;
+     * Array int[] {31, 37, 41, 43, 47}.
+     * <p>
+     * int a = 20;
+     * int b = 93;
+     * Array int[] {23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89}.
      */
 
     public static void main(String[] args) {
 
-        int a = 30;
-        int b = 50;
+        int a = 2;
+        int b = 1000;
 
         printArray(getArrayFromRangeWithOnlyPrimeNumbers(a, b));
     }
@@ -38,7 +37,8 @@ public class MakeArrayWithPrimeNumbersFromRange {
 
         for (int i = 0; i < loopLength; i++) {
 
-            if (isPrime(a + i)) arr[counter] = a + i;
+            if (isPrime(a + i))
+                arr[counter] = a + i;
             else continue;
 
             counter++;
@@ -49,11 +49,15 @@ public class MakeArrayWithPrimeNumbersFromRange {
 
     public static boolean isPrime(int number) {
 
-        if (number % 2 == 0) return number == 2;
+        if (number % 2 == 0)
+            return number == 2;
 
         int limit = (int) (0.1 + Math.sqrt(number));
 
-        for (int i = 3; i <= limit; i += 2) if (number % i == 0) return false;
+        for (int i = 3; i <= limit; i += 2) {
+            if (number % i == 0)
+                return false;
+        }
 
         return true;
     }
@@ -64,13 +68,16 @@ public class MakeArrayWithPrimeNumbersFromRange {
 
         do {
 
-            if (c == 0) System.out.print("Array int[] {");
-            else System.out.print(", ");
+            if (c == 0)
+                System.out.print("Array int[] {");
+            else
+                System.out.print(", ");
 
             System.out.print(arr[c]);
             c++;
 
-            if (c == arr.length) System.out.println("}.");
+            if (c == arr.length)
+                System.out.println("}.");
 
         } while (c < arr.length);
     }
